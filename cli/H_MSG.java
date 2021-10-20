723d0f31f59533405e8b947c6a9ea10d70c9da5a
DZ-FSDev <85122787+DZ-FSDev@users.noreply.github.com>
2021-10-20 09:54:53 -0500

    Update H_MSG.java

--- cli/H_MSG.java
+++ cli/H_MSG.java
@@ -4,19 +4,26 @@ import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.security.Security;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 /**
- * Performs any Java Security Standard Hash on UTF-8 Message 
+ * Performs any Java Security Standard Hash or Bouncy Castle Hashes on UTF-8 Message 
  * and optionally validates provided hash to true or false.
  * 
  * @see <a href=https://docs.oracle.com/javase/9/docs/specs/security/standard-names.html>
  *  		Java SE9 Security Standard Algorithm Names</a>
+ * @see <a href=https://www.bouncycastle.org/java.html>
+ * 			The Legion of the Bouncy Castle Java Cryptography APIs</a>
  * @author DZ-FSDev
- * @version 0.1.13
+ * @version 0.1.15
  * @since 16.0.1
  */
 public class H_MSG {
 	public static void main(String[] args) {
+		if(args.length==0)System.exit(0);
+		Security.addProvider(new BouncyCastleProvider());
 		final String ALG = args[0].toUpperCase();
 		try {
 			final String _H;
